7b41c5f357eafb426ebceead4c3f4914cd3aae7a
bineea <33547731+bineea@users.noreply.github.com>
2024-06-13 23:37:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# 注意
+国内无法访问openai的api（api.openai.com）；建议使用DevAGI（https://devcto.com）提供的openai代理地址和对应的api-key
---
+++ src/main/java/com/example/demo/AIController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class AIController {
+    private final ChatClient chatClient;
+
+    AIController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+    @GetMapping("/ai")
+    Map<String, String> completion(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of(
+                "completion",
+                chatClient.prompt()
+                        .user(message)
+                        .call()
+                        .content());
+    }
+}
